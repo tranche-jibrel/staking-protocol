6567eb6fe12a1354ff0656f66729212df8a53d23
Saurabh <sw@tranche.finance>
2021-02-21 21:05:56 +0400

    deploy and flatten updated

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 var Staking = artifacts.require('./Staking.sol');
+var { abi } = require('../build/contracts/IERC20.json');
 
 module.exports = async (deployer, network, accounts) => {
   // const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
@@ -10,10 +11,18 @@ module.exports = async (deployer, network, accounts) => {
     await deployer
       .deploy(Staking, 21022021, 1337, SLICEAddress, { from: tokenOwner })
       .then(console.log('Staking Contract Deployed: ', Staking.address));
+      
   } else if (network == 'kovan') {
     let { SLICEAddress } = process.env;
+    let SLICE = new web3.eth.Contract(abi, SLICEAddress)
+    const accounts = await web3.eth.getAccounts();
     const tokenOwner = accounts[0];
-    await deployer.deploy(Staking, 21022021, 1337, SLICEAddress, { from: tokenOwner });
+    const toWei = web3.utils.toWei;
+    
+    let SCInstance = await deployer.deploy(Staking, 21022021, toWei('1'), SLICEAddress, { from: tokenOwner });
+    SCInstance = await Staking.deployed();
+    await SCInstance.addStakableToken(SLICEAddress, toWei('1'), { from: tokenOwner })
+    await SLICE.methods.transfer(Staking.address, toWei('1000')).send({ from: tokenOwner });
     console.log('STAKING_ADDRESS=' + Staking.address);
     console.log('REACT_APP_STAKING_ADDRESS=' + Staking.address);
   }
---
+++ scripts/flatten.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+# This scripts can be used to create flat files which can be directly imported on Remix if needed.
+echo "Clearing existing flats"
+if [ -d dist ]; then
+    rm -rf dist
+fi
+
+mkdir dist
+# TO-DO: Comments (author, summary, Created On) should be handled better.
+
+# Staking
+echo "Flattening # Staking contract"
+npx truffle-flattener ./contracts/Staking.sol | awk '/SPDX-License-Identifier/&&c++>0 {next} 1' | awk '/pragma experimental ABIEncoderV2;/&&c++>0 {next} 1' | awk '/pragma solidity/&&c++>0 {next} 1' | awk '/author/&&c++>0 {next} 1' | awk '/summary/&&c++>0 {next} 1' | awk '/Created on/&&c++>0 {next} 1' | sed '/^[[:blank:]]*\/\/ File/d;s/#.*//' >./dist/Staking.sol
