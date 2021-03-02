191e6db375693377e7715851508125fabe5bafdf
Ayush Tiwari <at@tranche.finance>
2021-03-03 04:33:59 +0530

    Updated flattening script

--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -11,4 +11,12 @@ mkdir dist
 
 # Staking
 echo "Flattening # Staking contract"
-npx truffle-flattener ./contracts/Staking.sol | awk '/SPDX-License-Identifier/&&c++>0 {next} 1' | awk '/pragma experimental ABIEncoderV2;/&&c++>0 {next} 1' | awk '/pragma solidity/&&c++>0 {next} 1' | awk '/author/&&c++>0 {next} 1' | awk '/summary/&&c++>0 {next} 1' | awk '/Created on/&&c++>0 {next} 1' | sed '/^[[:blank:]]*\/\/ File/d;s/#.*//' >./dist/Staking.sol
+npx truffle-flattener ./contracts/Staking.sol | awk '/SPDX-License-Identifier/&&c++>0 {next} 1' | awk '/pragma experimental ABIEncoderV2;/&&c++>0 {next} 1' | awk '/pragma solidity/&&c++>0 {next} 1' | awk '/author/&&c++>0 {next} 1' | awk '/summary/&&c++>0 {next} 1' | awk '/Created on/&&c++>0 {next}  1' | sed '/^[[:blank:]]*\/\/ File/d;s/#.*//' >./dist/Staking.sol
+
+# StakingMilestones
+echo "Flattening # StakingMilestones contract"
+npx truffle-flattener ./contracts/StakingMilestones/StakingMilestones.sol | awk '/SPDX-License-Identifier/&&c++>0 {next} 1' | awk '/pragma experimental ABIEncoderV2;/&&c++>0 {next} 1' | awk '/pragma solidity/&&c++>0 {next} 1' | awk '/author/&&c++>0 {next} 1' | awk '/summary/&&c++>0 {next} 1' | awk '/Created on/&&c++>0 {next}  1' | sed '/^[[:blank:]]*\/\/ File/d;s/#.*//' >./dist/StakingMilestones.sol
+
+# YieldFarm
+echo "Flattening # YieldFarm contract"
+npx truffle-flattener ./contracts/StakingMilestones/YieldFarm.sol | awk '/SPDX-License-Identifier/&&c++>0 {next} 1' | awk '/pragma experimental ABIEncoderV2;/&&c++>0 {next} 1' | awk '/pragma solidity/&&c++>0 {next} 1' | awk '/author/&&c++>0 {next} 1' | awk '/summary/&&c++>0 {next} 1' | awk '/Created on/&&c++>0 {next}  1' | sed '/^[[:blank:]]*\/\/ File/d;s/#.*//' >./dist/YieldFarm.sol
