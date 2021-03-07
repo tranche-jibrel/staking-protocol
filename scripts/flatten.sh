2393caf4dedab6d841eb9ffc88a1f104b8478e1a
Saurabh <sw@tranche.finance>
2021-03-07 10:24:55 +0400

    updated flatten script

--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -11,7 +11,7 @@ mkdir dist
 
 # Staking
 echo "Flattening # Staking contract"
-npx truffle-flattener ./contracts/Staking.sol | awk '/SPDX-License-Identifier/&&c++>0 {next} 1' | awk '/pragma experimental ABIEncoderV2;/&&c++>0 {next} 1' | awk '/pragma solidity/&&c++>0 {next} 1' | awk '/author/&&c++>0 {next} 1' | awk '/summary/&&c++>0 {next} 1' | awk '/Created on/&&c++>0 {next}  1' | sed '/^[[:blank:]]*\/\/ File/d;s/#.*//' >./dist/Staking.sol
+npx truffle-flattener ./contracts/OldStaking/Staking.sol | awk '/SPDX-License-Identifier/&&c++>0 {next} 1' | awk '/pragma experimental ABIEncoderV2;/&&c++>0 {next} 1' | awk '/pragma solidity/&&c++>0 {next} 1' | awk '/author/&&c++>0 {next} 1' | awk '/summary/&&c++>0 {next} 1' | awk '/Created on/&&c++>0 {next}  1' | sed '/^[[:blank:]]*\/\/ File/d;s/#.*//' >./dist/Staking.sol
 
 # StakingMilestones
 echo "Flattening # StakingMilestones contract"
